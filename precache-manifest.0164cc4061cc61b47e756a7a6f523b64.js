908aac8913af6df6e00b14ffdb4d8c69d754f2e5
IbrahimSaffi <ibrahimsaffi@gmail.com>
2020-04-22 09:26:19 +0500

    Updates

---
+++ precache-manifest.0164cc4061cc61b47e756a7a6f523b64.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "693cd58e1232e5e70ccf6b383baacaa0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8cf97992ab0d8a7430e1",
+    "url": "/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "2755103398b0df17f864",
+    "url": "/static/css/main.48b60050.chunk.css"
+  },
+  {
+    "revision": "8cf97992ab0d8a7430e1",
+    "url": "/static/js/2.84d6e4e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.84d6e4e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2755103398b0df17f864",
+    "url": "/static/js/main.21a631ff.chunk.js"
+  },
+  {
+    "revision": "65c6441c6da975648346",
+    "url": "/static/js/runtime-main.a210e4d1.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
